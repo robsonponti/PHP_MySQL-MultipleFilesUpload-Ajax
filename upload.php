da0fdaafaeae2b66f7937292e1d3bbf380ea46f8
robsonponti <33622862+robsonponti@users.noreply.github.com>
2019-01-28 21:47:24 +0000

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,51 @@
+<?php
+
+//////////////////////////////////////////////////////////
+//////////////////by Robson Ponti////////////////////////
+////////////////////////////////////////////////////////
+
+require("db.php"); //your connection file here
+
+    if(count($_FILES['file']['name']) > 0){
+        //Loop through each file
+        for($i=0; $i<count($_FILES['file']['name']); $i++) {
+          //Get the temp file path
+            $tmpFilePath = $_FILES['file']['tmp_name'][$i];
+            $imageFileType = strtolower(pathinfo(basename($_FILES["file"]["name"][$i]),PATHINFO_EXTENSION));
+
+            //this line make sure we've a filepath
+            if($tmpFilePath != ""){
+            
+                //save the filename
+                $shortname = 'youcanwritesomethingifyouwant'. date("Y-m-d").".".$imageFileType; //you can customize this line, here is name of the file uploaded.
+
+                //save the url and the file
+               
+                $filePath = "youdirtoupload/"; //insert your dir
+                $finalPath = $filePath.$shortname;
+                
+                //Upload the file into the temp dir
+                if(move_uploaded_file($tmpFilePath, $finalPath)) {
+
+                    $files[] = $shortname;
+                    //here we insert into db 
+                    //$shortname for the filename
+                    //$filePath for the url file
+
+                    $stmt = $connection->prepare("INSERT INTO table (path, filename) VALUES(?, ?)");
+                    $stmt->bindParam(1, $filePath);
+                    $stmt->bindParam(2, $shortname);
+                    if($stmt->execute()){
+                      echo "Uploaded file with sucess";
+                    }
+                    else{
+                      echor "Sorry. Uploading file."; 
+                    }
+
+                }
+            }
+        }
+    }
+
+
+?>
